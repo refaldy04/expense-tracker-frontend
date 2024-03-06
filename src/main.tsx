86ec2c64ffa9a1d89ed3994f604e1a580bab1146
Refaldy <refaldybanggana@gmail.com>
2024-03-06 20:27:53 +0700

    setup deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { GridBackground } from "./components/ui/GridBackground.tsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://expense-tracker-backend-lake.vercel.app/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
