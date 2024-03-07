04f182d703a79e1b7d64d59053ffb94747035fb2
Refaldy <refaldybanggana@gmail.com>
2024-03-07 07:59:44 +0700

    link to backend

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { GridBackground } from "./components/ui/GridBackground.tsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://expense-tracker-backend-lake.vercel.app/graphql",
+  uri: "https://expense-tracker-backend-production-42e7.up.railway.app/graphql",
   cache: new InMemoryCache(),
   credentials: "include",
 });
